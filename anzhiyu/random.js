675cc2b454b432411b517e35c9f8c0968170fc66
mainideal <3253661722@qq.com>
2023-10-24 14:36:31 +0800

    Site updated: 2023-10-24 14:36:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/18/article-title/","2023/10/23/hello-world/","2023/10/24/基于hexo-github搭建博客/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/23/hello-world/","2023/10/18/article-title/","2023/10/24/基于hexo-github搭建博客/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
